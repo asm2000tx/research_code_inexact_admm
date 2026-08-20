5a3282cb7cdba11378d00254b0f0da20181097eb
axelsanchezmoreno <a_s1350@txstate.edu>
2026-08-19 22:58:32 -0500

    Added skeleton code to fista.py code

--- iadmm_jax/fista.py
+++ iadmm_jax/fista.py
@@ -1,2 +1,8 @@
-def prox_step():
-    return "Calling proximal step ...."
+class fista_const:
+    def __init__(self, A, b, beta):
+        self.A = A
+        self.b = b
+        self.beta = beta
+    
+    def prox_step(self):
+        print("Calling proximal step ....")
--- iadmm_jax/main.py
+++ iadmm_jax/main.py
@@ -1,26 +1,33 @@
 import numpy as np
-from fista import prox_step
+from fista import fista_const
 
 ## Inexact ADMM with relative error criterion written by Jiaxin, Anping, and Xiaobo
 
 def main():
     m, n = 250, 100
     A = np.random.randn(m, n)
+    beta = 1.5e3
     
     ## Define s-sparse vector x
     s = 25
-    x = np.zeros(n)
-    x[:s] = np.random.randn(s)
-    np.random.shuffle(x)
+    x_bar = np.zeros(n)
+    x_bar[:s] = np.random.randn(s)
+    np.random.shuffle(x_bar)
 
     ## Define the inexact solution
     delta = 1e-3
     eps = np.random.randn(m)
-    b = A @ x + delta * eps
+    b = A @ x_bar + delta * eps
 
-    message = prox_step()
-    print(f"Passed message: {message}")
-    
+    # Define x_p, y_p, and l_d 
+    x_p, y_p, l_d = np.zeros(n), np.zeros(m), np.zeros(m)
+
+    fista_step = fista_const(A, b, beta)
+
+    count = 10
+    while True:
+        count += 1
+        if count >= 10: break
     
 if __name__ == '__main__':
 	main()
