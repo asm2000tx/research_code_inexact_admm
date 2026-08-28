009373f6982b63c05949097c48cb7fd48368c38c
axelsanchezmoreno <a_s1350@txstate.edu>
2026-08-28 15:52:40 -0500

    Made minor changes to code and created distance norm to terminate FISTA algorithm.

--- iadmm_jax/fista.py
+++ iadmm_jax/fista.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class fista_const:
-    def __init__(self, A, y_p, l_d, b, w_1, beta, sigma_1):
+    def __init__(self, A, y_p, l_d, b, w_1, beta, sigma_1, xi_2):
         self.A = A
         self.y_p = y_p
         self.l_d = l_d
@@ -9,59 +9,64 @@ class fista_const:
         self.w_1 = w_1
         self.beta = beta
         self.sigma_1 = sigma_1
+        self.xi_2 = xi_2
+
+        # Second term inside the smooth function
+        self.c_k = self.b + (1 / self.beta) * self.l_d - self.y_p
 
         # Lipschitz constant
         self.L = self.beta * np.linalg.norm(self.A, 2) ** 2
 
     # Gradient of smooth term of objective function
-    def grad_func(self, v_step, c_k): return self.beta * self.A.T @ (self.A @ v_step - c_k)
+    def grad_func(self, curr_pt): return self.beta * self.A.T @ (self.A @ curr_pt - self.c_k)
 
     # Subgradient of L1-norm 
-    def soft_thresholding(self, grad_g): return np.sign(grad_g) * np.maximum(np.abs(grad_g) - (1/self.L), 0)
+    def soft_shrinkage(self, grad_g): return np.sign(grad_g) * np.maximum(np.abs(grad_g) - (1/self.L), 0)
 
-    def approx_cond(self, d_step, x_next):
+    def dist_cond(self, curr_pt, count): 
+        grad_g = curr_pt - (1/self.L) * self.grad_func(curr_pt)
+        curr_pt = self.soft_shrinkage(grad_g)
+        dist = np.linalg.norm(curr_pt, ord=np.inf)
+        if count % 250 == 0: print(f"||x_new||_inf | {dist}\n")
+        return (dist < self.xi_2)
+
+    def approx_cond(self, d_step, x_next, count):
         residual = self.A @ x_next + self.y_p - self.b
 
         lhs_term1 = (2.0 / self.beta) * np.abs((self.w_1 - x_next).T @ d_step)
         lhs_term2 = d_step.T @ d_step
-
         rhs_term = self.sigma_1 * (residual.T @ residual)
-
-        print(f"LHS - Term 1: {lhs_term1}")
-        print(f"LHS - Term 2: {lhs_term2}")
-        print(f"RHS         : {rhs_term}\n")
+        if count % 250 == 0:
+            print(f"Iteration #{count}:")
+            print(f" LHS sum      | {lhs_term1 + lhs_term2}")
+            print(f" RHS dist     | {rhs_term}")
 
         return (lhs_term1 + lhs_term2) <= rhs_term
     
     def prox_step(self):
         _, n = self.A.shape
-        c_k = self.b + (1 / self.beta) * self.l_d - self.y_p
 
-        # Start FISTA from a feasible, deterministic point.  In particular,
-        # x_curr and y_curr must represent the same iterate initially.
+        # primal variables and step size from the FISTA algorithm
         x_curr = np.zeros(n)
-        y_curr = x_curr.copy()
+        y_curr = np.zeros(n)
         t_curr = 1.0
 
         count = 1
         while True:
             # Gradient of smooth term at y_k with respect to x
-            grad_y = self.grad_func(y_curr, c_k)
+            grad_y = self.grad_func(y_curr)
+            x_next = self.soft_shrinkage(y_curr - (1/self.L) * grad_y)
 
-            # Proximal gradient step 
-            x_next = self.soft_thresholding(y_curr - (1/self.L) * grad_y)
-            
-            # Gradient of smooth term at x_k with respect to x
-            grad_x = self.grad_func(x_curr, c_k)
-
-            # Computing d_{1}^{k+1} from the FISTA subproblem. 
+            # Computing d_{1}^{k+1} from the FISTA subproblem
+            grad_x = self.grad_func(x_next)
             d_step = grad_x - self.L * (x_next - y_curr) - grad_y
 
             # Checking approximation threshold
-            if self.approx_cond(d_step, x_next): 
-                print(f"Threshold is met! Returning results after {count} steps ...\n")
+            if self.approx_cond(d_step, x_next, count) or self.dist_cond(x_next, count): 
+                print(f"Threshold is met! | Steps: {count}\n")
                 return d_step, x_next
 
+            # Condition(s) failed, computing the new local variables, t_next and y_next
             t_next = 0.5 * (1.0 + np.sqrt(1.0 + 4.0 * t_curr ** 2))
             y_next = x_next + ((t_curr - 1.0) / t_next) * (x_next - x_curr)
 
--- iadmm_jax/main.py
+++ iadmm_jax/main.py
@@ -3,11 +3,18 @@ from fista import fista_const
 
 ## Inexact ADMM with relative error criterion written by Jiaxin, Anping, and Xiaobo
 
-def threshold():
-    pass
+def threshold(A, x_p, y_p, y_prev, b, beta, count, xi_1):
+    stop_term1 = (np.linalg.norm(A @ x_p + y_p - b)) / (1.0 + np.linalg.norm(b))
+    stop_term2 = (np.linalg.norm(beta * A.T @ (y_p - y_prev))) / (1.0 + np.linalg.norm(y_prev))
+
+    stop_measure = max(stop_term1, stop_term2)
+    if (count % 100) == 0: print(f"Status on max-norm: {stop_measure}")
+    if stop_measure < xi_1: print(f"Condition met with stop_measure = {stop_measure}")
+
+    return (stop_measure < xi_1)
 
 def main():
-    m, n = 512, 2048
+    m, n = 1024, 4096
     A = np.random.randn(m, n)
     beta = 1.5e3
     
@@ -31,19 +38,31 @@ def main():
     w_1, w_2 = np.zeros(n), np.zeros(m)
 
     # Define random constants sigma_1 and sigma_2 whose sum is < 1
-    sigma_1 = 0.01
+    sigma_1 = 0.99
 
+    # Defining xi_1 and xi_2
+    xi_1 = 1e-4
+    xi_2 = 1e-8
+
+    count = 0
     while True:
         # Solving x-subproblem to compute x^{k+1}
-        fista_step = fista_const(A, y_p, l_d, b, w_1, beta, sigma_1)
+        fista_step = fista_const(A, y_p, l_d, b, w_1, beta, sigma_1, xi_2)
         d_1, x_p = fista_step.prox_step()
+
+        # Saving the previous y_p to use for termination of algorithm 1.
+        y_prev = y_p
 
         # Using closed-form solution to compute y^{k+1} 
         z = b + (1.0 / beta) * l_d - (A @ x_p)
         y_p = np.sign(z) * np.maximum(np.abs(z) - beta / mu, 0.0)
 
-        break # Temporary line, delete when complete
+        if threshold(A, x_p, y_p, y_prev, b, beta, count, xi_1): break
+
+        # Condition failed, incrementing count and updating l_d and w_1 variables. 
+        count += 1
+        l_d = l_d - beta * (A @ x_p + y_p - b)
+        w_1 = w_1 - beta * d_1
 
 if __name__ == '__main__':
 	main()
-
